a83e0c53d12f312af6c3c8dd7aba711e79b36e75
Marta Cruz <inmerset@gmail.com>
2022-11-12 03:05:54 +0100

    simply routes added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
