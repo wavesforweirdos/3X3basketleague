8883bef663811c0b761b6b23eed5eeda388734ba
Marta Cruz <inmerset@gmail.com>
2022-11-17 12:50:09 +0100

    some migrations corrected

---
+++ database/migrations/2022_11_15_094128_create_entities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->year('foundation_year');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('web')->nullable();
+            $table->string('country');
+            $table->string('city');
+            $table->foreignId('id_managers')
+            ->constrained('managers')
+            ->OnDelete('cascade')
+            ->OnUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entities');
+    }
+};
---
+++ database/migrations/2022_11_15_094210_create_games_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_leagues')
+                ->constrained('leagues')
+                ->OnDelete('cascade')
+                ->OnUpdate('cascade');
+            $table->foreignId('id_teams_local')
+                ->constrained('teams')
+                ->OnDelete('cascade')
+                ->OnUpdate('cascade');
+            $table->foreignId('id_teams_visiting')
+                ->constrained('teams')
+                ->OnDelete('cascade')
+                ->OnUpdate('cascade');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time')->nullable();
+            $table->string('score')->nullable();
+            $table->foreignId('referees_id')
+                ->constrained('referees')
+                ->OnDelete('cascade')
+                ->OnUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
