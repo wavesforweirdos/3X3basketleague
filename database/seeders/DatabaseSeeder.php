d6b63d23b98bc966735b78756947f8bb9e5fdc82
Marta Cruz <inmerset@gmail.com>
2022-11-17 13:02:37 +0100

    dbseeder & user modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,19 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\BasketCourt;
+use App\Models\Club;
+use App\Models\Entity;
+use App\Models\Referee;
+use App\Models\Game;
+use App\Models\League;
+use App\Models\Manager;
+use App\Models\Player;
+use App\Models\Team;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +26,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(9)->create();
+
+        //------All categories----/
+        \App\Models\Category::factory()->create([
+            'name' => 'senior',
+            'gender' => 'f',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'senior',
+            'gender' => 'm',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'senior',
+            'gender' => 'mix',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'junior',
+            'gender' => 'f',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'junior',
+            'gender' => 'm',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'junior',
+            'gender' => 'mix',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'infantil',
+            'gender' => 'f',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'infantil',
+            'gender' => 'm',
+        ]);
+        \App\Models\Category::factory()->create([
+            'name' => 'infantil',
+            'gender' => 'mix',
+        ]);
+
+        Club::factory(3)->create();
+        Referee::factory(6)->create();
+        Manager::factory(10)->create();
+        BasketCourt::factory(2)->create();
+        Entity::factory(2)->create();
+        League::factory(3)->create();
+        Team::factory(24)->create();
+        Game::factory(12)->create();
+        Player::factory(100)->create();
     }
 }
